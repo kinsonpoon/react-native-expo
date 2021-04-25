3c9877179a670610626bf28890a276426a0ac59a
kinsonp <kinson.poon@digidumpling.com>
2021-04-25 16:52:04 +0800

    first commit

---
+++ components/customHeader.tsx
@@ -0,0 +1,14 @@
+export default function header(){
+    return (
+        <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+
+
+
+    )
+
+
+}
